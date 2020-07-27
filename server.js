583f67a397c5c2dae7b2fc8f511b184e592a2d77
binjim2002 <binjim2002@yahoo.fr>
2020-07-27 14:18:27 -0400

    fixing port issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // html-calls
